ec3249640604d75869c1ea7eaa0f7f7e184f86e3
ErickHack <blackdragoneraj@gmail.com>
2023-08-18 18:44:14 +0500

    feat: added env, interceptor and axios

---
+++ src/shared/axios/index.ts
@@ -0,0 +1,63 @@
+import { AxiosRequestConfig } from "axios";
+import instance from "./interceptors";
+
+type DataType = "params" | "body";
+
+export function post(
+  url: string,
+  data?: any,
+  typeData?: DataType,
+  config?: AxiosRequestConfig
+) {
+  if (typeData === "params") {
+    return instance({
+      method: "POST",
+      url,
+      params: data,
+      ...config,
+    });
+  }
+
+  return instance({
+    method: "POST",
+    url,
+    data,
+    ...config,
+  });
+}
+
+export function put(
+  url: string,
+  data?: any,
+  typeData?: DataType,
+  config?: AxiosRequestConfig
+) {
+  if (typeData === "params") {
+    return instance({
+      method: "PUT",
+      url,
+      params: data,
+      ...config,
+    });
+  }
+
+  return instance({
+    method: "PUT",
+    url,
+    data,
+    ...config,
+  });
+}
+
+export function get(url: string, params: string, config: AxiosRequestConfig) {
+  return instance({
+    method: "GET",
+    url,
+    params,
+    ...config,
+  });
+}
+
+export function del(url: string, config: AxiosRequestConfig) {
+  return instance.delete(url, config);
+}
